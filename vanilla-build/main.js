29476a3102b9236d462844da7e093010fb424fdf
unknown <justinasve@gmail.com>
2024-04-15 23:06:41 +0300

    feat: register square by id and display an icon on clicked square

--- vanilla-build/main.js
+++ vanilla-build/main.js
@@ -1,15 +1,59 @@
 const App = {
   // All of our selected HTML elements
   $: {
-    menu: document.querySelector(".menu"),
-    menuItems: document.querySelector(".items")
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]'),
+  },
+
+  state: {
+    currentPlayer: 1,
   },
 
   init() {
+    App.registerEventListeners();
+  },
+
+  registerEventListeners() {
     App.$.menu.addEventListener("click", (event) => {
       App.$.menuItems.classList.toggle("hidden");
     });
-  }
+
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset the game");
+    });
+
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("Add a new round");
+    });
+
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        console.log(`Square with id ${event.target.id} was clicked`);
+        console.log(`Current player is ${App.state.currentPlayer}`);
+
+        if (event.target.hasChildNodes()) {
+          return;
+        }
+
+        const currentPlayer = App.state.currentPlayer;
+
+        const icon = document.createElement("i");
+
+        if (currentPlayer === 1) {
+          icon.classList.add("ph-bold", "ph-x", "yellow");
+        } else {
+          icon.classList.add("ph-bold", "ph-circle", "turquoise");
+        }
+
+        App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+
+        event.target.replaceChildren(icon);
+      });
+    });
+  },
 };
 
-window.addEventListener('load', App.init);
+window.addEventListener("load", App.init);
