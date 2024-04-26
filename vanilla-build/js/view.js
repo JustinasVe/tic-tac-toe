9eb8894082ce8a96a633975ce5863f2cd7c14311
unknown <justinasve@gmail.com>
2024-04-27 01:40:22 +0300

    refactor: call methods for the event listeners to be added and define DOM helper method toggleMenu

--- vanilla-build/js/view.js
+++ vanilla-build/js/view.js
@@ -3,6 +3,7 @@ export default class View {
 
   constructor() {
     this.$.menu = document.querySelector('[data-id="menu"]');
+    this.$.menuBtn = document.querySelector('[data-id="menu-btn"]');
     this.$.menuItems = document.querySelector('[data-id="menu-items"]');
     this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
     this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
@@ -11,7 +12,16 @@ export default class View {
     this.$.modalText = document.querySelector('[data-id="modal-text"]');
     this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
     this.$.turn = document.querySelector('[data-id="turn"]');
+
+    // UI-only event listeners
+    this.$.menuBtn.addEventListener("click", (event) => {
+      this.toggleMenu();
+    });
   }
+
+  /**
+   * Register all the event listeners
+   */
 
   bindGameResetEvent(handler) {
     this.$.resetBtn.addEventListener("click", handler);
@@ -25,5 +35,18 @@ export default class View {
     this.$.squares.forEach((square) => {
       square.addEventListener("click", handler);
     });
+  }
+
+  /**
+   * DOM helper methods
+   */
+  toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+
+    icon.classList.toggle("ph-caret-down");
+    icon.classList.toggle("ph-caret-up");
   }
 }
